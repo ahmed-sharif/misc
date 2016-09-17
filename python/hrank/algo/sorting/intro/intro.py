564cffbc18092a3bb9383458badccd07afebb98d
asharif <asharif@linkedin.com>
2016-09-17 14:22:47 -0700

    sorting intro

---
+++ python/hrank/algo/sorting/intro/intro.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/tutorial-intro
+
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (right + left) / 2
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+n = int(raw_input().strip())
+raw_input().strip()
+
+items = map(int, raw_input().strip().split())
+print binary_search(items, n)
+
+"""
+print binary_search([1, 4, 5, 7, 9, 12], 4)
+print binary_search([1, 4, 5, 7, 9, 12], 9)
+print binary_search([1, 4, 5, 7, 9, 12], 12)
+print binary_search([1, 4, 5, 7, 9, 12], 6)
+print binary_search([1, 4, 5, 7, 9, 12], 8)
+print binary_search([1, 4, 5, 7, 9, 12], 43)
+"""
