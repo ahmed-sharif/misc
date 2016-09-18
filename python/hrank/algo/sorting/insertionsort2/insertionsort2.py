c4b01d8437691346fe76864a5c2a86078d034110
asharif <asharif@linkedin.com>
2016-09-17 17:12:56 -0700

    insertion2

--- python/hrank/algo/sorting/insertionsort2/insertionsort2.py
+++ python/hrank/algo/sorting/insertionsort2/insertionsort2.py
@@ -4,26 +4,19 @@ def print_arr(items):
         print i,
     print
 
-
-
 def insertion_sort(items):
     total = len(items)
     for i in xrange(1, total):
-        item = items[i]
-        last_location = i
-        for j in xrange(i - 1, -1, -1):
-            if items[j] > item:
-                items[j + 1] = items[j]
-                last_location = j
-                # print_arr( items)
-        if last_location != i:
-            items[last_location] = item
-            # print_arr( items)
+        key = items[i]
+        j = i - 1
+        while j >= 0 and items[j] > key:
+            items[j + 1] = items[j]
+            j -= 1
+        items[j + 1] = key
         print_arr( items)
-
 
 n = int(raw_input().strip())
 items = map(int, raw_input().strip().split())
 
-# insertion_sort([2, 4, 6, 8, 3])
+# insertion_sort([1, 4, 3, 5, 6, 2])
 insertion_sort(items)
